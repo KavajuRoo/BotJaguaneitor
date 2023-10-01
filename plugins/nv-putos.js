f0e965bba63dea670749e20ed32b4d9e115ff005
KavajuRoo <145935009+KavajuRoo@users.noreply.github.com>
2023-10-01 15:32:33 -0300

    nv-putos.js

--- plugins/nv-putos.js
+++ plugins/nv-putos.js
@@ -1,1 +1,22 @@
+const handler = async (m, {conn}) => {
+  if (!db.data.chats[m.chat].audios) return;
+  if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
+  //const s = seconds: '1934.4'
+  const vn = './media/putos.mp3';
+  conn.sendPresenceUpdate('recording', m.chat);
+  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `putos.mp3`}, {quoted: m});
+};
+handler.customPrefix = /putos|putoz|reputos/
+handler.command = /^(putos|putitos|putos?$)/
+export default handler;
 
+/*
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/putos.mp3'
+conn.sendPresenceUpdate('recording', m.chat)
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `👑 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭 👑`, "body": `=> ᴀᴜᴅɪᴏ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴏ`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://github.com/BrunoSobrino/TheMystic-Bot-MD`, "showAdAttribution": true}}, seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+handler.customPrefix = /putos|putoz|reputos/
+handler.command = /^(putos|putitos|putos?$)/
+export default handler
+*/
